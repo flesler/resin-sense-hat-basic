0562dacb6c2bd09a1783a1f7e572d332561c54a6
Ariel Flesler <aflesler@gmail.com>
2017-06-16 15:53:52 -0300

    Separated the code into a couple of modules

--- index.js
+++ index.js
@@ -3,86 +3,36 @@
  * Press the joystick to cycle colors
  */
 
-const senseJoystick = require('sense-joystick');
-const leds = require('sense-hat-led').sync;
+const joystick = require('./lib/joystick');
+const canvas = require('./lib/canvas');
 
-const SIZE = 8;
+const dot = { x: canvas.SIZE / 2, y: canvas.SIZE / 2 };
+canvas.addDot(dot);
 
-const X = [0, 255, 0];
-const O = [0, 0, 255];
-const DOT = [255, 0, 0];
-const DIRS = {
-	left: { axis: 'x', dir: -1 },
-	right: { axis: 'x', dir: 1 },
-	up: { axis: 'y', dir: -1 },
-	down: { axis: 'y', dir: 1 },
-};
-
-
-const pixels = [
-	O, O, O, X, X, O, O, O,
-	O, O, X, O, O, X, O, O,
-	O, O, O, O, O, X, O, O,
-	O, O, O, O, X, O, O, O,
-	O, O, O, X, O, O, O, O,
-	O, O, O, X, O, O, O, O,
-	O, O, O, O, O, O, O, O,
-	O, O, O, X, O, O, O, O,
-];
-
-const dot = { x: SIZE / 2, y: SIZE / 2 };
-const control = { x: 0, y: 0 };
 let timeout;
 
-senseJoystick.getJoystick().then((joystick) => {
-	joystick.on('press', (direction) => {
-		const data = DIRS[direction];
-		if (data && control[data.axis] !== data.dir) {
-			control[data.axis] = data.dir;
-			// Ensure even a quick press is not ignored
-			updateDot();
-		}
-	});
-	joystick.on('release', (direction) => {
-		const data = DIRS[direction];
-		if (data && control[data.axis] === data.dir) {
-			control[data.axis] = 0;
-		}
-		if (direction === 'click') {
-			swapBackground();
-		}
-	});
-});
-
-function updateDot() {
-	if (control.x || control.y) {
-		for (const axis in control) {
-			let val = dot[axis] + control[axis];
-			if (val < 0) val += SIZE;
-			if (val >= SIZE) val = 0;
-			dot[axis] = val;
-		}
-		draw();
+function tick() {
+	if (joystick.pressed) {
+		canvas.cycle();
 	}
+	move('x');
+	move('y');
+
 	clearTimeout(timeout);
-	timeout = setTimeout(updateDot, 300);
+	timeout = setTimeout(tick, 300);
 }
 
-function draw() {
-	leds.setPixels(pixels);
-	leds.setPixel(dot.x, dot.y, DOT);
+function move(axis) {
+	if (joystick[axis]) {
+		let n = dot[axis] + joystick[axis];
+		if (n < 0) n += canvas.SIZE;
+		if (n >= canvas.SIZE) n = 0;
+		dot[axis] = n;
+		canvas.draw();
+	}
 }
 
-function cycle(arr) {
-	arr.push(arr.shift());
-}
-
-function swapBackground() {
-	cycle(X);
-	cycle(O);
-	cycle(DOT);
-	draw();
-}
-
-updateDot();
-draw();
+tick();
+canvas.draw();
+// Ensure even a quick press is not ignored
+joystick.on('changed', tick);

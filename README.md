ad058d714ee391ae9ec8dce44eefe5f020bb84a8
Ariel Flesler <aflesler@gmail.com>
2017-06-16 16:09:26 -0300

    Added super rainbow when holding joystick, accelerometer implemention is wip

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 My first experiment using SenseHAT's LEDs and joystick, powered by [resin.io](https://resin.io/)
 
+## Control
+
+- Use the joystick to move the dot
+- Press the joystick button to cycle colors
+- Hold button for rainbow
+- (WIP) Use the accelerometer to move the dot
+
 ## Demo
 
 ![](images/demo.gif)
--- index.js
+++ index.js
@@ -1,33 +1,43 @@
-/**
- * Use the joystick to move the dot
- * Press the joystick to cycle colors
- */
-
+const util = require('./lib/util');
 const joystick = require('./lib/joystick');
 const canvas = require('./lib/canvas');
+const accelerometer = require('./lib/accelerometer');
 
 const dot = { x: canvas.SIZE / 2, y: canvas.SIZE / 2 };
 canvas.addDot(dot);
 
 let timeout;
+let holding;
 
 function tick() {
 	if (joystick.pressed) {
+		if (holding) {
+			// Increase FPS for an awesome rainbow
+			schedule(1);
+			return canvas.drawRainbow();
+		}
 		canvas.cycle();
+		holding = true;
+	} else if (holding) {
+		canvas.draw();
+		holding = false;
 	}
+	accelerometer.update();
 	move('x');
 	move('y');
+	schedule(300);
+}
 
+function schedule(time) {
 	clearTimeout(timeout);
-	timeout = setTimeout(tick, 300);
+	timeout = setTimeout(tick, time);
 }
 
 function move(axis) {
-	if (joystick[axis]) {
-		let n = dot[axis] + joystick[axis];
-		if (n < 0) n += canvas.SIZE;
-		if (n >= canvas.SIZE) n = 0;
-		dot[axis] = n;
+	const delta = joystick[axis] + accelerometer[axis];
+	if (delta) {
+		const n = dot[axis] + delta;
+		dot[axis] = util.clamp(n, 0, canvas.SIZE - 1);
 		canvas.draw();
 	}
 }
